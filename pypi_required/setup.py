d906c192845fe2905b0c0030280fc72ebe4f497a
Tyler Watkins <tylerwatkins101@gmail.com>
2019-02-08 16:17:05 -0200

    Add files via upload

---
+++ pypi_required/setup.py
@@ -0,0 +1,12 @@
+# Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
+
+from setuptools import setup
+
+setup(name='regression_diagnostics',
+      version='0.1',
+      description='Package for doing Regression Diagnostics in Python',
+      packages=['regression_diagnostics'],
+      author = 'Tyler Watkins',
+      author_email = 'tylerwatkins101@gmail.com',
+      zip_safe=False)
